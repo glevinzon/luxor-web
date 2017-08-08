087c9b826dd6da1858a58814483f87f05f4531af
Glevinzon <glevinzon.zeal@gmail.com>
2017-08-08 14:57:27 +0800

    fix

--- src/routes/Home/components/Header/Header.js
+++ src/routes/Home/components/Header/Header.js
@@ -59,7 +59,7 @@ class Header extends Component {
               <div className='header-content'>
                 <div className='header-content-inner'>
                   {this.state.title && (<h1>{this.state.title}</h1>)}
-                  {this.state.title && (<a href='#download' className='btn btn-outline btn-xl page-scroll' onClick={this.openModal} >Reserve Now!</a>)}
+                  {this.state.title && (<a href='#features' className='btn btn-outline btn-xl page-scroll' onClick={this.openModal} >Reserve Now!</a>)}
                 </div>
               </div>
             </div>
