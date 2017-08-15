f2dd53e87121f8ec27300af3af238158e6f59002
Glevinzon <glevinzon.zeal@gmail.com>
2017-08-15 15:46:34 +0800

    filtered in tabs reservations by status

--- src/routes/Reservations/components/Reservation/ReservationTable.js
+++ src/routes/Reservations/components/Reservation/ReservationTable.js
@@ -3,6 +3,7 @@ import Immutable from 'immutable'
 import moment from 'moment'
 import cx from 'classnames'
 import SweetAlert from 'react-bootstrap-sweetalert'
+import { Tabs, Tab } from 'react-bootstrap'
 
 class ReservationTable extends Component {
   constructor (props) {
@@ -80,33 +81,111 @@ class ReservationTable extends Component {
         this.setState({approveAlert: (<SweetAlert
           info
           showCancel
-          confirmBtnText='Yes, approve and reject other.'
-          confirmBtnBsStyle='info'
-          cancelBtnBsStyle='default'
+          confirmBtnText='Yes, approve'
+          confirmBtnBsStyle='success'
+          cancelBtnText='Reject'
+          cancelBtnBsStyle='danger'
           title='Are you sure?'
-          onConfirm={e => { this.handleUpdateStatus(reserveId) }}
-          onCancel={e => { this.setState({approveAlert: null}) }}
+          onConfirm={e => { this.handleUpdateStatus(reserveId, 'approve') }}
+          onCancel={e => { this.handleUpdateStatus(reserveId, 'reject') }}
         >
-        This will approve selected request and will reject all other pending requests on {room.get('name')}({room.get('type')}).
+        This will approve selected request on {room.get('name')}({room.get('type')}).
         </SweetAlert>)})
       }}>Pending</button>)
     } else if (status == '1') {
       indicator = (<button type='button' className='btn btn-sm btn-pill btn-success'>Approved</button>)
+    } else if (status == '11') {
+      indicator = (<button type='button' className='btn btn-sm btn-pill btn-info'>Archieved</button>)
     }
     return indicator
   }
 
-  handleUpdateStatus = (reserveId) => {
-    this.props.updateReservationStatus(reserveId, 'approve')
+  handleUpdateStatus = (reserveId, status) => {
+    this.props.updateReservationStatus(reserveId, status)
     this.setState({approveAlert: null})
+  }
+
+  handleSelect = (e) => {
+    event.preventDefault()
+    this.setState({ selectedTab: e })
+  }
+
+  renderTable = (data, roomsData, filter) => {
+    return (
+      <div className='table-full'>
+        <div className='table-responsive'>
+          <table className='table' data-sort='table'>
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Customer Name</th>
+                <th>Note</th>
+                <th>Contact</th>
+                <th>Email</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Room Type</th>
+                <th>Room Name</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data && (data.map(reserve => {
+                return (
+                  roomsData && roomsData.map(room => {
+                    if ((reserve.get('room_id') == room.get('id') && (reserve.get('status') == filter))) {
+                      return (
+                        <tr key={reserve.get('id')}>
+                          <td><a href='#'>{reserve.get('code')}</a></td>
+                          <td>{reserve.get('fullName')}</td>
+                          <td>{reserve.get('note')}</td>
+                          <td>{reserve.get('contact')}</td>
+                          <td>{reserve.get('email')}</td>
+                          <td>{moment(reserve.get('startDate')).format('dddd, MM/DD/YY')}</td>
+                          <td>{moment(reserve.get('endDate')).format('dddd, MM/DD/YY')}</td>
+                          <td>{room.get('type')}</td>
+                          <td>{room.get('name')}</td>
+                          <td>{this.renderStatus(reserve.get('status'), room, reserve.get('id'))}</td>
+                          <td>
+                            <div className='btn-group'>
+                              <button type='button' className='btn btn-primary-outline'>
+                                <span className='icon icon-pencil' />
+                              </button>
+                              <button type='button' className='btn btn-primary-outline' onClick={e => { this.handleDelete(reserve.get('code')) }}>
+                                <span className='icon icon-erase' />
+                              </button>
+                            </div></td>
+                        </tr>
+                      )
+                    }
+                  })
+                )
+              }))}
+            </tbody>
+          </table>
+        </div>
+        <div className='text-center'>
+          <ul className='pagination'>
+            <li>
+              <a aria-label='Previous' onClick={e => this.handlePaginationClick('prev')} style={{display: cx({'none': this.state.page < 2 || this.props.fetchingReservations})}} >
+                <span aria-hidden='true'>&laquo;</span>
+              </a>
+            </li>
+            <li>
+              <a aria-label='Next' onClick={e => this.handlePaginationClick('next')} style={{display: cx({'none': (data && data.size === 0) || this.props.fetchingReservations})}} >
+                <span aria-hidden='true'>&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
   }
 
   render () {
     let { reserves, fetchingReservations, rooms } = this.props
     if (reserves) {
-      var total = reserves.get('total')
-      var currentPage = reserves.get('currentPage')
-      var lastPage = reserves.get('lastPage')
       var data = reserves.get('data')
     }
 
@@ -119,74 +198,16 @@ class ReservationTable extends Component {
         {this.state.delete}
         {this.state.alert}
         {this.state.approveAlert}
-        <div className='table-full'>
-          <div className='table-responsive'>
-            <table className='table' data-sort='table'>
-              <thead>
-                <tr>
-                  <th>Code</th>
-                  <th>Customer Name</th>
-                  <th>Note</th>
-                  <th>Contact</th>
-                  <th>Email</th>
-                  <th>Start Date</th>
-                  <th>End Date</th>
-                  <th>Room Type</th>
-                  <th>Room Name</th>
-                  <th>Status</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {data && (data.map(reserve => {
-                  return (
-                    roomsData && roomsData.map(room => {
-                      if (reserve.get('room_id') == room.get('id')) {
-                        return (
-                          <tr key={reserve.get('id')}>
-                            <td><a href='#'>{reserve.get('code')}</a></td>
-                            <td>{reserve.get('fullName')}</td>
-                            <td>{reserve.get('note')}</td>
-                            <td>{reserve.get('contact')}</td>
-                            <td>{reserve.get('email')}</td>
-                            <td>{moment(reserve.get('startDate')).format('dddd, MM/DD/YY')}</td>
-                            <td>{moment(reserve.get('endDate')).format('dddd, MM/DD/YY')}</td>
-                            <td>{room.get('type')}</td>
-                            <td>{room.get('name')}</td>
-                            <td>{this.renderStatus(reserve.get('status'), room, reserve.get('id'))}</td>
-                            <td>
-                              <div className='btn-group'>
-                                <button type='button' className='btn btn-primary-outline'>
-                                  <span className='icon icon-pencil' />
-                                </button>
-                                <button type='button' className='btn btn-primary-outline' onClick={e => { this.handleDelete(reserve.get('code')) }}>
-                                  <span className='icon icon-erase' />
-                                </button>
-                              </div></td>
-                          </tr>
-                        )
-                      }
-                    })
-                  )
-                }))}
-              </tbody>
-            </table>
-          </div>
+
+        <div className='hr-divider'>
+          <Tabs bsStyle='nav nav-pills hr-divider-content hr-divider-tab' activeKey={this.state.selectedTab || 'pending'} onSelect={this.handleSelect} id='controlled-tab-example'>
+            <Tab style={{textAlign: 'left'}} key='pending' eventKey='pending' title='PENDING'>{this.renderTable(data, roomsData, 0)}</Tab>
+            <Tab style={{textAlign: 'left'}} key='approved' eventKey='approved' title='APPROVED'>{this.renderTable(data, roomsData, 1)}</Tab>
+            <Tab style={{textAlign: 'left'}} key='rejected' eventKey='rejected' title='REJECTED'>{this.renderTable(data, roomsData, 10)}</Tab>
+            <Tab style={{textAlign: 'left'}} key='archieved' eventKey='archieved' title='ARCHIEVED'>{this.renderTable(data, roomsData, 11)}</Tab>
+          </Tabs>
         </div>
-        <div className='text-center'>
-          <ul className='pagination'>
-            <li>
-              <a aria-label='Previous' onClick={e => this.handlePaginationClick('prev')} style={{display: cx({'none': this.state.page < 2 || fetchingReservations})}} >
-                <span aria-hidden='true'>&laquo;</span>
-              </a>
-            </li>
-            <li>
-              <a aria-label='Next' onClick={e => this.handlePaginationClick('next')} style={{display: cx({'none': (data && data.size === 0) || fetchingReservations})}} >
-                <span aria-hidden='true'>&raquo;</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+
       </div>
     )
   }
