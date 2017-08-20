f9de5c09a49976434847c415fa5fc5af25036d7c
Glevinzon <glevinzon.zeal@gmail.com>
2017-08-20 15:26:17 +0800

    revert back changes on main.js file

--- server/main.js
+++ server/main.js
@@ -7,6 +7,8 @@ import serve from 'koa-static'
 import proxy from 'koa-proxy'
 import _debug from 'debug'
 import config from '../config'
+import webpackDevMiddleware from './middleware/webpack-dev'
+import webpackHMRMiddleware from './middleware/webpack-hmr'
 
 const debug = _debug('app:server')
 const paths = config.utils_paths
@@ -28,9 +30,6 @@ app.use(convert(historyApiFallback({
 // Apply Webpack HMR Middleware
 // ------------------------------------
 if (config.env === 'development') {
-  import webpackDevMiddleware from './middleware/webpack-dev'
-  import webpackHMRMiddleware from './middleware/webpack-hmr'
-
   const compiler = webpack(webpackConfig)
 
   // Enable webpack-dev and webpack-hot middleware
